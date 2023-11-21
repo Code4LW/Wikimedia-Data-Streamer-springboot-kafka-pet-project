06852b07733df48d004fdcac9644f8292f57c705
Zanggar <zangar992103@gmail.com>
2023-11-21 17:52:52 +0600

    package directories are corrected

--- kafkaproducer-wikimedia/kafkaconsumer-database/src/main/java/net/kafka/springboot/repo/WikiDataRepository.java
+++
@@ -1,7 +0,0 @@
-package net.kafka.springboot.repo;
-
-import net.kafka.springboot.model.WikiData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WikiDataRepository extends JpaRepository<WikiData,Long> {
-}
